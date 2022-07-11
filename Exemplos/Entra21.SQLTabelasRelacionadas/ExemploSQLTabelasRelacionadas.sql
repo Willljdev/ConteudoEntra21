24a4a0f01506d3422ee10f8c16b858768f44d768
willljdev <willljdev@gmail.com>
2022-07-11 20:31:46 -0300

    Criação do Arquivo ExemploSQLTabelasRelacionadas da aula de SQL Server

---
+++ Exemplos/Entra21.SQLTabelasRelacionadas/ExemploSQLTabelasRelacionadas.sql
@@ -0,0 +1,42 @@
+﻿CREATE TABLE pecas(
+	id INTEGER PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(50),
+	tipo INTEGER,
+	preco_unitario DECIMAL(6,2));
+
+CREATE TABLE clientes(
+	id INTEGER PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(40),
+	cpf VARCHAR(14));
+
+CREATE TABLE enderecos(
+	id INTEGER PRIMARY KEY IDENTITY(1,1), --PK
+	id_cliente INTEGER NOT NULL,  --FK
+	cep VARCHAR(9) NOT NULL,
+	estado VARCHAR(2) NOT NULL,
+	cidade VARCHAR(30) NOT NULL,
+	bairro VARCHAR(30) NOT NULL,
+	logradouro VARCHAR(40) NOT NULL,
+	numero VARCHAR(10) NOT NULL,
+	descricao TEXT,
+
+	--FOREIGN KEY nome_coluna_tabela_atual REFERENCES nome_tabela_pai(nome_coluna_pai_pk)
+	FOREIGN KEY(id_cliente) REFERENCES clientes(id));
+	
+CREATE TABLE pedidos(
+	id INTEGER PRIMARY KEY IDENTITY(1,1),
+	id_cliente INTEGER NOT NULL,
+	status TINYINT NOT NULL, --0..255
+	data_efetivacao_compra DATETIME2,
+	data_criacao DATETIME2 NOT NULL
+);
+
+CREATE TABLE pedidos_pecas(
+	id INTEGER PRIMARY KEY IDENTITY(1,1), --PK
+	id_peca INTEGER, --FK tabela peças
+	id_pedido INTEGER, --FK tabela pedido
+
+	quantidade SMALLINT,
+
+	FOREIGN KEY (id_pedido) REFERENCES pedidos(id),
+	FOREIGN KEY(

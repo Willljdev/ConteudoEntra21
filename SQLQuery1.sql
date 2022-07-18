638eadaa6d841dca256fbf4de99352f4209e3b2c
willljdev <willljdev@gmail.com>
2022-07-18 20:38:44 -0300

    SQLQuery1

---
+++ SQLQuery1.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO editoras (nome, apelido, data_criacao, faturamento) 
+	VALUES 
+		('DC', 'DC Comics', '1939-01-20', 2727000000.00),
+		('Marvel', 'Marvel Comics', '1939-01-01', 3200000000.00),
+		(

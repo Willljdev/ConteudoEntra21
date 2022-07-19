ef2fa97acab3ef6140f81430b528a1d070604a63
willljdev <willljdev@gmail.com>
2022-07-18 22:11:23 -0300

    SQL SERVER - Exercício criado. Foram criados as classes Cidade e UnidadeFederativa.

---
+++ Entra21.BancoDados02.Ado.Net.Exercicio.Form/Models/Cidade.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.BancoDados02.Ado.Net.Exercicio.Form.Models
+{
+    public class Cidade
+    {
+        public int Id { get; set; }
+        public UnidadeFederativa UnidadeFederativa { get; set; }
+        public string Nome { get; set; }
+        public int QuantidadeHabitantes { get; set; }
+        public DateTime DataHoraFundacao { get; set; }
+        public decimal Pib { get; set; }
+    }
+}
